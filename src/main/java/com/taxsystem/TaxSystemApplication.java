364bbb5e5f724775359160cc8f4798925a17c5b4
nicolasholanda <nikolas.nhm14@gmail.com>
2026-03-16 08:47:11 -0300

    Initial commit

---
+++ src/main/java/com/taxsystem/TaxSystemApplication.java
@@ -0,0 +1,12 @@
+package com.taxsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxSystemApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TaxSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/taxsystem/TaxSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.taxsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TaxSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
