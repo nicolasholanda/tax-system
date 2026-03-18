2f4bc657b3b38ca742f633e423bc40c7e861966f
nicolasholanda <nikolas.nhm14@gmail.com>
2026-03-18 09:59:37 -0300

    Added findByProductIdAndStateIdAndYear

--- src/main/java/com/taxsystem/repository/TaxRateRepository.java
+++ src/main/java/com/taxsystem/repository/TaxRateRepository.java
@@ -3,5 +3,9 @@ package com.taxsystem.repository;
 import com.taxsystem.entity.TaxRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaxRateRepository extends JpaRepository<TaxRate, Long> {
+
+    Optional<TaxRate> findByProductIdAndStateIdAndYear(Long productId, Long stateId, Integer year);
 }
